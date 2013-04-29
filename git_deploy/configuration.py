be86f9873daaeb522f3d537ebd8010bd71c0da93
Christophe Benz <christophe.benz@gmail.com>
2013-04-30 00:18:47 +0200

    Fix merge conf algorithm.

--- git_deploy/configuration.py
+++ git_deploy/configuration.py
@@ -37,25 +37,24 @@ log = logging.getLogger(os.path.basename(__file__))
 def get_conf(config_dir_path):
     if not os.path.isdir(config_dir_path):
         return None
-    conf = {}
+    conf_dicts = []
     for config_file_path in glob.iglob(os.path.join(config_dir_path, '*.json')):
         log.debug(u'get_conf: config_file_path = {}'.format(config_file_path))
         with open(config_file_path) as config_file:
             config_file_str = config_file.read()
         try:
-            config_file_json = json.loads(config_file_str)
+            conf_dict = json.loads(config_file_str)
         except ValueError, exc:
             log.error(u'Failed to decode repository JSON configuration for file "{}": {}'.format(
                 config_file_path, unicode(exc)))
             return None
-        config_file_conf, errors = conv.json_values_to_conf(config_file_json)
-        if errors is None:
-            conf.update(config_file_conf)
-            log.debug(u'get_conf: updated conf = {}'.format(conf))
-        else:
+        conf_data, errors = conv.json_values_to_conf(conf_dict)
+        if errors is not None:
             log.error(u'Repository configuration errors for file "{}": {}'.format(config_file_path, errors))
             return None
-    log.debug(u'get_conf: final conf = {}'.format(conf))
+        conf_dicts.append(conf_data)
+    conf = merge_conf_dicts(conf_dicts)
+    log.debug(u'get_conf: conf = {}'.format(conf))
     return conf
 
 
@@ -66,3 +65,14 @@ def get_repo_conf(conf, repo_url):
                 log.debug(u'get_repo_conf: repo_alias = {}, repo_conf = {}'.format(repo_alias, repo_conf))
                 return repo_alias, repo_conf
     return None, None
+
+
+def merge_conf_dicts(conf_dicts):
+    hooks = {}
+    repositories = {}
+    for conf_dict in conf_dicts:
+        if conf_dict.get('hooks'):
+            hooks.update(conf_dict['hooks'])
+        if conf_dict.get('repositories'):
+            repositories.update(conf_dict['repositories'])
+    return {'hooks': hooks, 'repositories': repositories}
